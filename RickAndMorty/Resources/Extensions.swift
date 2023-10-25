fb9c4ee94f2a4936017e6884408e65e5a37f48c3
Pedro Mendez <tostado_mt@hotmail.com>
2023-10-24 20:08:49 -0600

    vid6

---
+++ RickAndMorty/Resources/Extensions.swift
@@ -0,0 +1,16 @@
+//
+//  Extensions.swift
+//  RickAndMorty
+//
+//  Created by PEDRO MENDEZ on 24/10/23.
+//
+
+import UIKit
+
+extension UIView {
+    func addSubview(_ views: UIView...) {
+        views.forEach({
+            addSubview($0)
+        })
+    }
+}
