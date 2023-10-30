f0504d7fcda4f1db5a118231bc03dcdb069a3940
Pedro Mendez <tostado_mt@hotmail.com>
2023-10-30 16:56:58 -0600

    vid13

--- RickAndMorty/Controllers/Other/RMCharacterDetailViewController.swift
+++ RickAndMorty/Controllers/Other/RMCharacterDetailViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 final class RMCharacterDetailViewController: UIViewController {
     
     private let viewModel: RMCharacterDetailViewViewModel
+    private let detailView = RMCharacterDetailView()
+    
     init(viewModel: RMCharacterDetailViewViewModel) {
         self.viewModel = viewModel
         super.init(nibName: nil, bundle: nil)
@@ -24,5 +26,27 @@ final class RMCharacterDetailViewController: UIViewController {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
         title = viewModel.title
+        view.addSubview(detailView)
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action,
+                                                            target: self,
+                                                            action: #selector(didTapShare))
+        addConstraints()
+    }
+    
+    
+    
+    @objc
+    private func didTapShare() {
+        //button to share character info
+        
+    }
+    
+    private func addConstraints() {
+        NSLayoutConstraint.activate([
+            detailView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            detailView.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor),
+            detailView.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor),
+            detailView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+        ])
     }
 }
--- RickAndMorty/ViewModels/RMCharacterDetailViewViewModel.swift
+++ RickAndMorty/ViewModels/RMCharacterDetailViewViewModel.swift
@@ -13,7 +13,29 @@ final class RMCharacterDetailViewViewModel {
     init(character: RMCharacter) {
         self.character = character
     }
+    
+    private var requestUrl: URL? {
+        return URL(string: character.url)
+    }
+    
     public var title: String {
         character.name.uppercased()
     }
 }
+
+
+//public func fetchCharacterData() {
+//    print(character.url)
+//    guard let url = requestUrl,
+//          let request = RMRequest(url: url) else {
+//        return
+//    }
+//    RMService.shared.execute(request,
+//                             expecting: RMCharacter  .self) { result in
+//        switch result {
+//        case .success(let success):
+//            print(String(describing: success ))
+//        case .failure(let failure):
+//            print(String(describing: failure))
+//        }
+//    }
