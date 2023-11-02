d3abb2491f97cb0f4c1fc3f87ed96a97736c23c7
Pedro Mendez <tostado_mt@hotmail.com>
2023-11-02 16:50:46 -0600

    vid20

--- RickAndMorty/Views/CharacterDetails/RMCharacterEpisodeCollectionViewCell.swift
+++ RickAndMorty/Views/CharacterDetails/RMCharacterEpisodeCollectionViewCell.swift
@@ -12,8 +12,10 @@ final class RMCharacterEpisodeCollectionViewCell: UICollectionViewCell {
     
     override init(frame: CGRect) {
         super.init(frame: frame)
+        contentView.backgroundColor  = .systemBlue
+        contentView.layer.cornerRadius = 8
     }
-    
+     
     required init?(coder: NSCoder) {
         fatalError()
     }
@@ -27,6 +29,11 @@ final class RMCharacterEpisodeCollectionViewCell: UICollectionViewCell {
     }
     
     public func configure(with viewModel: RMCharacterEpisodeCollectionViewCellViewModel) {
-        
+        viewModel.registerForData {data in
+            print(data.name)
+            print(data.air_date)
+            print(data.episode)
+        }
+        viewModel.fetchEpisode()
     }
 }
