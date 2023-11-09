6ac867cea1dca56b99ce5b2c2619c07f7d8870cc
Pedro Mendez <tostado_mt@hotmail.com>
2023-11-09 16:53:50 -0600

    vid31

---
+++ RickAndMorty/Views/SwiftUI/RMSettingsView.swift
@@ -0,0 +1,47 @@
+//
+//  RMSettingsView.swift
+//  RickAndMorty
+//
+//  Created by PEDRO MENDEZ on 09/11/23.
+//
+
+import SwiftUI
+
+struct RMSettingsView: View {
+    
+    let viewModel: RMSettingsViewViewModel
+    
+    init(viewModel: RMSettingsViewViewModel) {
+        self.viewModel = viewModel
+    }
+   
+    
+    var body: some View {
+            List(viewModel.cellViewModels) { viewModel in
+                HStack {
+                    if let image = viewModel.image {
+                        Image(uiImage: image)
+                            .resizable()
+                            .renderingMode(.template)
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 30, height: 30)
+                            .padding(.trailing ,3)
+                            .background(Color(viewModel.iconContainerColor))
+                            .cornerRadius(10)
+                    }
+                    Text(viewModel.title)
+                        .padding(.leading, 7)
+                }
+                .padding()
+        }
+    }
+}
+
+struct RMSettingsView_Previews: PreviewProvider {
+    static var previews: some View {
+        
+        RMSettingsView(viewModel: .init(cellViewModels: RMSettingsOption.allCases.compactMap({
+            return RMSettingsCellViewModel(type: $0)
+        })))
+    }
+}
