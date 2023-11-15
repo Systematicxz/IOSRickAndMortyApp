5b9cd67feb7a517a1df0d176b257f09d17f4707d
Pedro Mendez <tostado_mt@hotmail.com>
2023-11-14 19:35:32 -0600

    vid44

--- RickAndMorty/ViewModels/Search/RMSearchViewViewModel.swift
+++ RickAndMorty/ViewModels/Search/RMSearchViewViewModel.swift
@@ -9,8 +9,35 @@ import Foundation
 
 final class RMSearchViewViewModel {
     let config: RMSearchViewController.Config
+    private var optionMap: [RMSearchInputViewViewModel.DynamicOption: String] = [:]
+    private var searchText = ""
     
+    private var optionMapUpdateblock: (((RMSearchInputViewViewModel.DynamicOption, String)) -> Void)?
+    
+    // MARK: - Init
     init(config: RMSearchViewController.Config) {
         self.config = config
+    }
+    
+    // MARK: - Public
+    
+    public func executeSearch() {
+        
+    }
+    
+    public func set(querry text: String) {
+        self.searchText = text
+    }
+    
+    
+    public func set(value: String, for option: RMSearchInputViewViewModel.DynamicOption) {
+        optionMap[option] = value
+        let tuple = (option, value)
+        optionMapUpdateblock?(tuple)
+    }
+    
+    public func registerOptionChangeBlock(_ block: @escaping((RMSearchInputViewViewModel.DynamicOption, String)) -> Void
+    ) {
+        self.optionMapUpdateblock = block
     }
 }
--- RickAndMorty/Views/Searchç/RMSearchInputView.swift
+++ RickAndMorty/Views/Searchç/RMSearchInputView.swift
@@ -35,6 +35,8 @@ final class RMSearchInputView: UIView {
         }
     }
     
+    private var stackView: UIStackView?
+    
     // MARK: - Init
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -76,7 +78,7 @@ final class RMSearchInputView: UIView {
     
     private func createOptionSelectionViews(options: [RMSearchInputViewViewModel.DynamicOption]) {
         let stackView = createOptionStackView()
-        
+        self.stackView = stackView
         for x in 0..<options.count {
             let option = options[x]
             let button = createButton(with: option, tag: x)
@@ -124,5 +126,27 @@ final class RMSearchInputView: UIView {
     
     public func presentKeyBoard(){
         searchBar.becomeFirstResponder()
+    }
+    
+    public func update(
+        option: RMSearchInputViewViewModel.DynamicOption,
+        value: String
+    ) {
+        // Update options
+        guard let buttons = stackView?.arrangedSubviews as? [UIButton],
+              let allOptions = viewModel?.options,
+                let index = allOptions.firstIndex(of: option) else {
+            return
+        }
+        buttons[index].setAttributedTitle(
+            NSAttributedString(
+                string: value.uppercased(),
+                attributes: [
+                    .font: UIFont.systemFont(ofSize: 18, weight: .medium),
+                    .foregroundColor: UIColor.link
+                ]
+            ),
+            for: .normal
+        )
     }
 }
