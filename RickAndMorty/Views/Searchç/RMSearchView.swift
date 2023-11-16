43604a6377358292983f03ac68e3dc9275e8ded6
Pedro Mendez <tostado_mt@hotmail.com>
2023-11-15 20:31:49 -0600

    vid46

--- RickAndMorty/Views/Searchç/RMSearchView.swift
+++ RickAndMorty/Views/Searchç/RMSearchView.swift
@@ -92,4 +92,10 @@ extension RMSearchView: RMSearchInputViewDelegate {
     func rmSearchInputView(_ inputView: RMSearchInputView, didSelectOption option: RMSearchInputViewViewModel.DynamicOption) {
         delegate?.rmSearchView(self, didSelectOption: option)
     }
+    func rmSearchInputView(_ inputView: RMSearchInputView, didChangeSearchText text: String) {
+        viewModel.set(querry: text)
+    }
+    func rmSearchInputViewDidTapSearchKeyboardButton(_ inputView: RMSearchInputView) {
+        viewModel.executeSearch()
+    }
 }
